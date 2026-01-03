dbe719439ed6bb532bf7c40528ce8dfc89d080b5
ygr774734227-tech <ygr774734227@gmail.com>
2026-01-04 04:56:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // 下面这段配置是为了兼容 CodeSandbox 的老习惯
-  // 允许在 .js 文件里写 JSX 代码
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.js?$/, 
-    exclude: []
-  },
 })
